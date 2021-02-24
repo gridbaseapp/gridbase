de44be9720379e4181362cc559068ec23cfc0fa5
Kamil Giszczak <beerkg@gmail.com>
2021-02-24 13:24:18 +0100

    Remove unused import

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import NewConnection from './NewConnection';
 import styles from './Launcher.scss'
 import { IConnectionDetails } from '../connection';
