25ceb7bfa8d5bf2a095cc49fe6e05ce2d8281716
Kamil Giszczak <beerkg@gmail.com>
2022-09-14 18:34:35 +0200

    Redesign Splash screen

--- src/renderer/app/components/Splash.tsx
+++ src/renderer/app/components/Splash.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import styles from './Splash.scss';
 
 export function Splash() {
-  return <div>Loading...</div>
+  return (
+    <div className={styles.splash}>
+      <div className={styles.panel}>
+        <h1>GridBase</h1>
+        <div className={styles.gridbaseLoader}>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+          <div></div><div></div><div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
