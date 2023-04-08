802e16e2f3c4b0f9e058fab6b583d46339d343f7
Kamil Giszczak <beerkg@gmail.com>
2023-04-08 15:26:14 +0200

    Setup eslint

---
+++ packages/desktop/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
+  ignorePatterns: ['dist/**/*', '.eslintrc.js'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  }
+}
